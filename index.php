a3967eaf08eeb8268833c8caa5087941d82452a5
lizasetre <60122669+lizasetre@users.noreply.github.com>
2020-10-24 15:11:51 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,4 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!-- Generator: Adobe Illustrator 22.1.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
-<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-	 viewBox="0 0 5000 5000" style="enable-background:new 0 0 5000 5000;" xml:space="preserve">
-<style type="text/css">
-	.st0{fill:none;}
-</style>
-<g id="Layer_1">
-</g>
-<g id="Layer_2">
-	<polygon class="st0 zero" points="586.1,878 997.8,493.5 931.5,270.2 771,186.5 586.2,284.7 398.4,184.5 240.8,270.2 178,490 	"/>
-</g>
-<g id="Layer_3">
-	<line class="st0 one" x1="994.3" y1="493.5" x2="178" y2="490"/>
-</g>
-<g id="Layer_4">
-	<polyline class="st0 two" points="768.5,184.1 586.2,866.2 401.3,186.5 	"/>
-</g>
-<g id="Layer_5">
-</g>
-</svg>
+<div class="icon calendar">
+  <i></i>
+  <i></i>
+</div>
