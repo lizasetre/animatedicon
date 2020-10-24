6212f3f75e72e7b9d5149bc53e3e944c41f0a650
lizasetre <60122669+lizasetre@users.noreply.github.com>
2020-10-24 15:15:55 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,26 @@
-<div class="icon calendar">
-  <i></i>
-  <i></i>
-</div>
+
+<svg
+   width="164"
+   height="164"
+   viewBox="0 0 20 20">
+  <g
+     id="layer1"
+     transform="translate(0,-280.06665)">
+    <path
+       style="opacity:1;fill:#000000;fill-opacity:1;stroke:none;stroke-width:0.49873331;stroke-linecap:round;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+       d="m 7.7879314,283.46841 c 0.417343,0 0.8346861,0 1.2520291,0 0,3.29938 0,6.59876 0,9.89814 -0.417343,0 -0.8346861,0 -1.2520291,0 0,-3.29938 0,-6.59876 0,-9.89814 z"
+       id="rect4489" />
+    <path
+       style="opacity:1;fill:#000000;fill-opacity:1;stroke:none;stroke-width:0.49873331;stroke-linecap:round;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+       d="m 9.3737164,283.46841 c 0.4173429,0 0.8346856,0 1.2520286,0 0,3.29938 0,6.59876 0,9.89814 -0.417343,0 -0.8346857,0 -1.2520286,0 0,-3.29938 0,-6.59876 0,-9.89814 z"
+       id="rect4491"/>
+    <path
+       style="opacity:1;fill:#000000;fill-opacity:1;stroke:none;stroke-width:0.49873331;stroke-linecap:round;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+       d="m 6.2021456,283.46841 c 0.417343,0 0.834686,0 1.252029,0 0,3.29938 0,6.59876 0,9.89814 -0.417343,0 -0.834686,0 -1.252029,0 0,-3.29938 0,-6.59876 0,-9.89814 z"
+       id="rect4487"/>
+    <path
+       style="opacity:1;fill:none;fill-opacity:1;stroke:#000000;stroke-width:0.49873331;stroke-linecap:round;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+       d="m 15.804999,288.4718 c 0.12431,3.21895 -2.321152,6.15938 -5.335794,7.06693 -3.4680064,1.10145 -7.5643678,-0.94758 -8.7856138,-4.37123 -1.21998851,-2.97836 -0.2603006,-6.7642 2.426283,-8.61423 2.9004361,-2.1109 7.3971318,-1.79397 9.7880618,0.9538 1.204396,1.33848 1.89909,3.1258 1.906966,4.92698"
+       id="path4493" />
+  </g>
+</svg>
